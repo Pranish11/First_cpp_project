5310d2e9e14196520de705c016ccf2fb4cf34ae5
Pranish <pranishdhital9@gmail.com>
2025-12-22 16:37:33 +0545

    Updated

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "SFML/Audio/Listener.hpp"
+
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u{800, 600}), "Title",sf::Style::Titlebar|sf::Style::Close);
-
+    sf::Clock clock;
 
     sf::Texture texture("../assets/Sprite-0001.png");
     sf::Texture ground ("../assets/Ground.png");
@@ -15,9 +17,22 @@ int main() {
 
     sprite001.setPosition({40.f,30.f});
     sprite001.setScale({2.f,2.f});
+    sprite001.setOrigin({
+        sprite001.getLocalBounds().size.x/2.f,
+        sprite001.getLocalBounds().size.y/2.f});
+    //* Going to use this "facingLeft" for checking if the sprite is facing right or left
+    bool facingLeft = true;
 
+
+    //ground
     Ground_01.setPosition({0.f,0.f});
     Ground_01.setScale({10.f,5.f});
+
+    //hitboxes for player
+    sf::RectangleShape PlayerHitbox;
+    PlayerHitbox.setSize(sf::Vector2f(sprite001.getGlobalBounds().size.x,sprite001.getGlobalBounds().size.y));
+    PlayerHitbox.setPosition(sprite001.getPosition());
+    PlayerHitbox.setFillColor(sf::Color{255,0,0,100}); //for testing
 
 
     while (window.isOpen())
@@ -26,16 +41,24 @@ int main() {
         //movement
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            sprite001.move({-0.5f,0.f});
+            sprite001.move({-0.2f,0.f});
+            if (!facingLeft) {
+                sprite001.setScale({2.f,2.f});
+                facingLeft = true;
+            }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            sprite001.move({0.5f,0.f});
+            sprite001.move({0.2f,0.f});
+            if (facingLeft) {
+                sprite001.setScale({-2.f,2.f});
+                facingLeft = false;
+            }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            sprite001.move({0.0f,-0.5f});
+            sprite001.move({0.0f,-0.2f});
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            sprite001.move({0.0f,0.5f});
+            sprite001.move({0.0f,0.2f});
         }
 
             // handle events
