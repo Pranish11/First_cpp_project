5be31d070d4a86477fcab29ac654cfdbc6050495
Pranish <pranishdhital9@gmail.com>
2025-12-24 12:12:47 +0545

    Update on attack

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u{800, 600}), "Title",sf::Style::Titlebar|sf::Style::Close);
     sf::Clock clock;
 
+
     sf::Texture texture("../assets/Sprite-0001.png");
     sf::Texture ground ("../assets/Ground.png");
+    sf::Texture Attack_001("../assets/Attack_1.png");
     sf::Sprite sprite001(texture);
     sf::Sprite Ground_01(ground);
-
+    sf::Sprite Attack_01(Attack_001);
 
     sprite001.setPosition({40.f,30.f});
     sprite001.setScale({2.f,2.f});
@@ -30,15 +32,19 @@ int main() {
     Ground_01.setPosition({0.f,0.f});
     Ground_01.setScale({10.f,5.f});
 
+    //attack
+    // Attack_01.setPosition({static_cast<float>(sprite001.getPosition().x),static_cast<float>(sprite001.getPosition().y)});
+
     //hitboxes for player
     sf::RectangleShape PlayerHitbox;
-    PlayerHitbox.setSize(SpriteBounds.size);         // width/height
+    PlayerHitbox.setSize(SpriteBounds.size);
     PlayerHitbox.setPosition(SpriteBounds.position);
     PlayerHitbox.setFillColor(sf::Color{255,0,0,100}); //for testing
 
 
     while (window.isOpen())
     {
+        float dt = clock.getElapsedTime().asSeconds();
 
         //movement
 
@@ -80,13 +86,14 @@ int main() {
         }
 
         //* Update Hitbox of the player
-        sf::FloatRect SpriteBounds = sprite001.getGlobalBounds();
         PlayerHitbox.setSize(bounds.size);
         PlayerHitbox.setPosition(bounds.position);
 
+        if (dt >=1.f/60.f) {
             // handle events
             while (auto event = window.pollEvent())
             {
+
                 // on close button release
                 if (event->is<sf::Event::Closed>())
                 {
@@ -101,12 +108,18 @@ int main() {
                     window.setView(sf::View(sf::FloatRect({0.f, 0.f}, sf::Vector2f(resized->size))));
                 }
             }
+        }
         window.clear(sf::Color::White);
         window.draw(Ground_01);
         //Render ground and background first so the character don't disappear
         window.draw(sprite001);
         //player hitbox
         window.draw(PlayerHitbox);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::P)) {
+                Attack_01.move({-0.1f,0.f});
+            window.draw(Attack_01);
+            Attack_01.setPosition({bounds.position});
+        }
         window.display();
     }
 
